6cec56dcec6ab55d8ef4307cad0b5591d0ea00ab
Joao Vitor <joaovitors@outlook.com>
2023-03-20 00:52:30 -0300

    feat(backend): prepara o serviço para receber testes

---
+++ app/backend/tests/unit/test.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Teste simples', function () {
+  it('Deve ser verdadeiro', function () {
+    expect(true).to.be.true;
+  });
+});
